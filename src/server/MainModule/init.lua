681a6daaa3e2327c22b9bca68c4dd15e4a444428
Frrz <daniel@celebrar.fot.br>
2023-01-09 19:22:40 -0300

    Update init.lua

--- src/server/MainModule/init.lua
+++ src/server/MainModule/init.lua
@@ -4,7 +4,6 @@ local Players = game:GetService("Players")
 local HttpService = game:GetService("HttpService")
 
 local Promise = require(ReplicatedStorage.Promise)
-local Signal = require(ReplicatedStorage.Signal)
 
 --[=[
   @class AvatarEditor
@@ -15,36 +14,7 @@ local Signal = require(ReplicatedStorage.Signal)
 local AvatarEditor = {}
 AvatarEditor.__index = AvatarEditor
 
-
-
-local Cached = {}
-local CacheSignal = Signal.new()
-local CacheLimit = 50
-
-local LoadedInventories = {}
-
-CacheSignal:Connect(function(Url, Response)
-	if Cached[Url] == nil and #Cached <= CacheLimit then
-       Cached[Url] =  { 
-        Data = Response,
-        Index = #Cached
-       }
-    else 
-		for Url, Data in pairs(Cached) do
-            if Data.Index == 1 then
-                table.remove(Cached, Url)
-            end
-        end
-    end
-end)
-
-function AvatarEditor:GetCached(Url) 
-    if Cached[Url] then 
-        return Cached[Url]
-    else 
-        return  nil   
-    end    
-end
+local Loaded = {}
 
 --[=[
      Returns a HumanoidDescription which specifies everything equipped for the avatar of the user specified by the passed in Id. Also includes scales and body colors.
@@ -53,7 +23,6 @@ end
      @within AvatarEditor	
      @param Id number --The userId of the specified player.
      @server
-     @tag Not cached 
      @return Promise<HumanoidDescription?>
 ]=]
 
@@ -81,7 +50,6 @@ end
      @within AvatarEditor	
      @param PlayerName string -- The username of the specified player.
      @server
-     @tag Not cached 
      @return Promise<HumanoidDescription?>
 ]=]
 
@@ -110,7 +78,6 @@ end
      @within AvatarEditor	
      @param PlayerToBeMorphId Player -- The player that will morph into another one
      @param PlayerToBeMorphed Player -- The player that will have its HumanoidDescription applied in the other one
-     @tag Not cached 
      @server
 ]=]
 
@@ -141,7 +108,6 @@ end
      @method RemovePlayerAccessories
      @within AvatarEditor	
      @param Player Player -- The Player to remove accessories
-	 @tag Not cached     
      @server
 ]=]
 
@@ -159,7 +125,6 @@ end
      @within AvatarEditor	
      @param Player Player -- The player to add accessory
      @param Accessory Accessory -- The accessory to add into the player's character 
-     @tag Not cached
      @server
 ]=]
 
@@ -177,7 +142,6 @@ end
      @within AvatarEditor	
      @param Id number --The userId of the specified player.
      @server
-     @tag Not cached
      @return Promise<table?>
 ]=]
 
@@ -204,32 +168,24 @@ end
      @within AvatarEditor	
      @param Id number --The userId of the specified player.
      @server
-     @tag Cached
      @return Promise<table?>
 ]=]
 
 
 function AvatarEditor:GetPlayerGears(Id : number)
-    local Url = "https://inventory.roproxy.com/v2/users/%s/inventory?assetTypes=Gear&limit=100&sortOrder=Asc"
+    return Promise.new(function(Resolve, Reject)
+        local Url = "https://inventory.roproxy.com/v2/users/%s/inventory?assetTypes=Gear&limit=100&sortOrder=Asc"
+        local Response
+        local Sucess, Error = pcall(function()
+            Response = HttpService:JSONDecode(HttpService:GetAsync(Url:format(Id)))
+        end)
 
-        return Promise.new(function(Resolve, Reject)
-		if AvatarEditor:GetCached(Url) then
-               return Resolve(AvatarEditor:GetCached(Url))
-            else
+        if not Sucess then
+            return Reject(Error)
+        end
 
-            local Response
-            local Sucess, Error = pcall(function()
-                Response = HttpService:JSONDecode(HttpService:GetAsync(Url:format(Id)))
-            end)
-    
-            if not Sucess then
-                return Reject(Error)
-            end
-             
-			CacheSignal:Fire(Url, Response.data)
-			return Resolve(Response)
-          end
-        end):catch(warn)
+        return Resolve(Response)
+    end):catch(warn)
 end
 
 --[=[
@@ -240,24 +196,15 @@ end
      @param Id number --The userId of the specified player.
      @param Cursor string 
      @server
-     @tag Cached
      @return Promise<table?>
 ]=]
 
 
 function AvatarEditor:GetUserInventoryAsync(Id : number, Cursor : string)
-	if Cursor ~= "" then 
-		Cursor = "&cursor="..Cursor
-	end
-	
-    local Url = "https://inventory.roproxy.com/v2/users/"..tostring(Id).."/inventory?assetTypes=Gear%2C,Hat%2C,TShirt%2C,Shirt%2C,Pants%2C,Head%2C,Face%2C,Animation%2C,Torso%2C,RightArm%2C,LeftArm%2C,LeftLeg%2C,RightLeg%2C,Package%2C,HairAccessory%2C,FaceAccessory%2C,NeckAccessory%2C,ShoulderAccessory%2C,FrontAccessory%2C,BackAccessory%2C,WaistAccessory%2C,ClimbAnimation%2C,DeathAnimation%2C,FallAnimation%2C,IdleAnimation%2C,JumpAnimation%2C,RunAnimation%2C,SwimAnimation%2C,WalkAnimation%2C,PoseAnimation%2C,TShirtAccessory%2C,ShirtAccessory%2C,PantsAccessory%2C,JacketAccessory%2C,SweaterAccessory%2C,ShortsAccessory%2C,LeftShoeAccessory%2C,RightShoeAccessory%2C,DressSkirtAccessory%2C,EyebrowAccessory%2C,EyelashAccessory%2C,MoodAnimation%2C,DynamicHead"..Cursor.."&limit=100&sortOrder=Asc"
     return Promise.new(function(Resolve, Reject)
-		if AvatarEditor:GetCached(Url) then
-			return Resolve(AvatarEditor:GetCached(Url))
-         else
 
-        if LoadedInventories[Id] == nil then 
-            LoadedInventories[Id] = {
+        if Loaded[Id] == nil then 
+            Loaded[Id] = {
                 ["Gear"] = {},
                 ["Hat"] = {},
                 ["TShirt"] = {},
@@ -303,12 +250,14 @@ function AvatarEditor:GetUserInventoryAsync(Id : number, Cursor : string)
                 ["DynamicHead"] = {}
             }
         end
-
+        if Cursor ~= "" then 
+            Cursor = "&cursor="..Cursor
+        end
 
         local Response
 
         local Sucess, Error = pcall(function()
-            Response = HttpService:JSONDecode(HttpService:GetAsync(Url))
+            Response = HttpService:JSONDecode(HttpService:GetAsync("https://inventory.roproxy.com/v2/users/"..tostring(Id).."/inventory?assetTypes=Gear%2C,Hat%2C,TShirt%2C,Shirt%2C,Pants%2C,Head%2C,Face%2C,Animation%2C,Torso%2C,RightArm%2C,LeftArm%2C,LeftLeg%2C,RightLeg%2C,Package%2C,HairAccessory%2C,FaceAccessory%2C,NeckAccessory%2C,ShoulderAccessory%2C,FrontAccessory%2C,BackAccessory%2C,WaistAccessory%2C,ClimbAnimation%2C,DeathAnimation%2C,FallAnimation%2C,IdleAnimation%2C,JumpAnimation%2C,RunAnimation%2C,SwimAnimation%2C,WalkAnimation%2C,PoseAnimation%2C,TShirtAccessory%2C,ShirtAccessory%2C,PantsAccessory%2C,JacketAccessory%2C,SweaterAccessory%2C,ShortsAccessory%2C,LeftShoeAccessory%2C,RightShoeAccessory%2C,DressSkirtAccessory%2C,EyebrowAccessory%2C,EyelashAccessory%2C,MoodAnimation%2C,DynamicHead"..Cursor.."&limit=100&sortOrder=Asc"))
         end)
 
 
@@ -316,22 +265,20 @@ function AvatarEditor:GetUserInventoryAsync(Id : number, Cursor : string)
             return Reject(Error)
         elseif Sucess and Response then
             for _, Item in ipairs(Response.data) do 
-                if LoadedInventories[Id][Item.assetType] ~= nil then
-                    table.insert(LoadedInventories[Id][Item.assetType], Item)
+                if Loaded[Id][Item.assetType] ~= nil then
+                    table.insert(Loaded[Id][Item.assetType], Item)
                 end
             end
         end
 
-			CacheSignal:Fire(Url, Response.data)
         return Resolve({
-            ["data"] = LoadedInventories[Id],
+            ["data"] = Loaded[Id],
             ["nextPageCursor"] = Response.nextPageCursor,
             ["previousPageCursor"] = Response.previousPageCursor
         })
-    end
+
     end):catch(warn)
 end    
-
 
 --[=[
      Returns whether the inventory of given PlayerId contains an item, given the ID. 
@@ -341,17 +288,12 @@ end
      @param Id number --The userId of the specified player.
      @param ItemId string   --The itemId of the specified item.
      @server
-     @tag Cached
      @return Promise<boolean?>
 ]=]
 
 function AvatarEditor:CheckIfUserOwnsItem(Id : number, ItemId : number) : boolean
-    local Url = "https://inventory.roproxy.com/v1/users/"..tostring(Id).."/items/Asset/"..tostring(ItemId).."/is-owned"
     return Promise.new(function(Resolve, Reject)
-		if AvatarEditor:GetCached(Url) then
-			return Resolve(AvatarEditor:GetCached(Url))
-     else
-
+        local Url = "https://inventory.roproxy.com/v1/users/"..tostring(Id).."/items/Asset/"..tostring(ItemId).."/is-owned"
         local Response 
         local Sucess, Error = pcall(function()
             Response = HttpService:JSONDecode(HttpService:GetAsync(Url))
@@ -361,9 +303,7 @@ function AvatarEditor:CheckIfUserOwnsItem(Id : number, ItemId : number) : boolea
             return Reject(Error)
         end    
 
-			CacheSignal:Fire(Url, Response.data)
-			return Resolve(Response)
-    end
+        return Resolve(Response)
     end):catch(warn)
 end    
 
@@ -378,33 +318,28 @@ end
      @param Subcategory string -- Check link below for more details
      @param CreatorName string -- Check link below for more details
      @server
-     @tag Cached
      @return Promise<table?>
 ]=]
 
 function AvatarEditor:GetItemsByCategory(Category : string, Cursor : string, SortType : string, Subcategory : string, CreatorName: string)
-	if Category ~= "" then
-		Category = "Category="..Category
-	end
-	if Cursor ~= "" then
-		Cursor = "&Cursor="..Cursor
-	end
-	if SortType ~= "" then
-		SortType = "&SortType="..SortType
-	end
-	if Subcategory ~= "" then
-		Subcategory = "&Subcategory="..Subcategory
-	end
-	if CreatorName ~= "" then
-		CreatorName = "&CreatorName="..CreatorName
-	end
-	
-	local Url = "https://catalog.roproxy.com/v1/search/items/details?"..Category..Cursor..SortType..Subcategory..CreatorName.."&Limit=30"
-
     return Promise.new(function(Resolve, Reject)
-		if AvatarEditor:GetCached(Url) then
-           return Resolve(AvatarEditor:GetCached(Url))
-     else
+        if Category ~= "" then
+            Category = "Category="..Category
+        end
+        if Cursor ~= "" then
+            Cursor = "&Cursor="..Cursor
+        end
+        if SortType ~= "" then
+            SortType = "&SortType="..SortType
+        end
+        if Subcategory ~= "" then
+            Subcategory = "&Subcategory="..Subcategory
+        end
+        if CreatorName ~= "" then
+            CreatorName = "&CreatorName="..CreatorName
+        end
+
+        local Url = "https://catalog.roproxy.com/v1/search/items/details?"..Category..Cursor..SortType..Subcategory..CreatorName.."&Limit=30"
         local Response
         local Sucess, Error = pcall(function()
             Response = HttpService:JSONDecode(HttpService:GetAsync(Url, true))
@@ -413,15 +348,14 @@ function AvatarEditor:GetItemsByCategory(Category : string, Cursor : string, Sor
         if not Sucess then
             return Reject(Error)
         elseif Sucess and Response then    
-				CacheSignal:Fire(Url, Response.data)
-				return Resolve({
+            return Resolve({
                 ["data"] = Response.data,
                 ["nextPageCursor"] = Response.nextPageCursor,
                 ["previousPageCursor"] = Response.previousPageCursor
             })
         end
 
-    end
+
     end):catch(warn)
 end
 
@@ -431,19 +365,14 @@ end
      @method GetRolimonsLimitedsInfos
      @within AvatarEditor	
      @server
-     @tag Cached
      @return Promise<table?>
 ]=]
 
 function AvatarEditor:GetRolimonsLimitedsInfos()
-	local Url = "https://www.rolimons.com/itemapi/itemdetails"
     return Promise.new(function(Resolve, Reject)
-		if AvatarEditor:GetCached(Url) then
-           return Resolve(AvatarEditor:GetCached(Url))
-     else
         local Response 
         local Sucess, Error = pcall(function()
-				Response = HttpService:JSONDecode(HttpService:GetAsync(Url))
+            Response = HttpService:JSONDecode(HttpService:GetAsync("https://www.rolimons.com/itemapi/itemdetails"))
         end)
 
         if not Sucess then
@@ -478,13 +407,9 @@ function AvatarEditor:GetRolimonsLimitedsInfos()
                     ["Item_Id"] = Id
                 }
             end
-			end
-			
-			CacheSignal:Fire(Url, Response.items)
-			return Resolve(Data)
-    end
+        end
+        return Resolve(Data)
     end):catch(warn)
 end
-
 
 return AvatarEditor
